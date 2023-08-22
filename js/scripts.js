0af4ef2e05ba71558d95cf6e84a9e15b94457b07
Jonas Silva <jonas86@edu.unisinos.br>
2023-08-22 15:20:33 -0300

    [delete] feat [refactor] criando novo script sem jquery

---
+++ js/scripts.js
@@ -0,0 +1,77 @@
+// Função que preenche um número com zeros à esquerda para atingir o tamanho desejado
+function pad(num, size) {
+  let s = num + "";
+  while (s.length < size) s = "0" + s;
+  return s;
+}
+
+// Espera até que o DOM esteja completamente carregado para executar o código
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtém elementos do DOM necessários
+  let ultimaSenhaNormal = document.getElementById("ultimaSenhaNumero");
+  let ultimaSenhaPreferencial = document.getElementById("ultimaSenhaNumeroPreferencial");
+
+  // Adiciona um ouvinte de evento ao botão "Direita"
+  document.getElementById("botaoDireita").addEventListener("click", function () {
+    // Obtém elementos do DOM necessários para manipulação
+    let senhaAtual = document.getElementById("senhaAtualNumero");
+    let senhaNormal = document.getElementById("senhaNormal");
+    let audioChamada = document.getElementById("audioChamada");
+
+    // Atualiza a última senha normal e gera uma nova senha
+    ultimaSenhaNormal.innerHTML = senhaAtual.innerHTML;
+    let senha = parseInt(senhaNormal.value) + 1;
+    senhaAtual.innerHTML = pad(senha, 4);
+    senhaNormal.value = pad(senha, 4);
+
+    // Reproduz o áudio de chamada
+    audioChamada.play();
+  });
+
+  // Adiciona um ouvinte de evento ao botão "Esquerda"
+  document.getElementById("botaoEsquerda").addEventListener("click", function () {
+    // Obtém elementos do DOM necessários para manipulação
+    let senhaAtual = document.getElementById("senhaAtualNumero");
+    let senhaNormal = document.getElementById("senhaNormal");
+
+    // Atualiza a última senha normal e gera uma nova senha decrementada
+    ultimaSenhaNormal.innerHTML = senhaAtual.innerHTML;
+    let senha = parseInt(senhaNormal.value) - 1;
+    senhaAtual.innerHTML = pad(senha, 4);
+    senhaNormal.value = pad(senha, 4);
+  });
+
+  // Adiciona um ouvinte de evento ao botão "Cima"
+  document.getElementById("botaoCima").addEventListener("click", function () {
+    // Obtém elementos do DOM necessários para manipulação
+    let senhaAtual = document.getElementById("senhaAtualNumero");
+    let senhaPrior = document.getElementById("senhaPrioridade");
+    let audioChamada = document.getElementById("audioChamada");
+
+    // Atualiza a última senha preferencial e gera uma nova senha de prioridade
+    ultimaSenhaPreferencial.innerHTML = senhaAtual.innerHTML;
+    let senha = parseInt(senhaPrior.value.replace("P", "")) + 1;
+    senhaAtual.innerHTML = "P" + pad(senha, 3);
+    senhaPrior.value = "P" + pad(senha, 3);
+
+    // Reproduz o áudio de chamada
+    audioChamada.play();
+  });
+
+  // Adiciona um ouvinte de evento ao botão "Baixo"
+  document.getElementById("botaoBaixo").addEventListener("click", function () {
+    // Obtém elementos do DOM necessários para manipulação
+    let senhaAtual = document.getElementById("senhaAtualNumero");
+    let senhaPrior = document.getElementById("senhaPrioridade");
+    let proximaSenha = document.getElementById("proximaSenhaNumero");
+
+    // Atualiza a última senha preferencial e gera uma nova senha de prioridade decrementada
+    ultimaSenhaPreferencial.innerHTML = senhaAtual.innerHTML;
+    let senha = parseInt(senhaPrior.value.replace("P", "")) - 1;
+    senhaAtual.innerHTML = "P" + pad(senha, 3);
+    senhaPrior.value = "P" + pad(senha, 3);
+
+    // Atualiza a próxima senha normal
+    proximaSenha.innerHTML = pad(parseInt(document.getElementById("senhaNormal").value) + 1, 4);
+  });
+});
