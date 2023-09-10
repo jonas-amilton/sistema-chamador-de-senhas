4c1ddb1fabf6ed5093d3dd191c85414edde84768
Jonas Silva <jonas86@edu.unisinos.br>
2023-09-09 21:27:00 -0300

    [fix]: correcao de erros

--- src/app/features/management-system.js
+++ src/app/features/management-system.js
@@ -12,32 +12,30 @@ function select(currentPassword, size) {
 // Espera até que o DOM esteja completamente carregado para executar o código
 document.addEventListener("DOMContentLoaded", function () {
   // Obtém elementos do DOM necessários
-  let ultimaSenhaNormal = document.getElementById("ultimaSenhaNumero");
-  let ultimaSenhaPreferencial = document.getElementById(
-    "ultimaSenhaNumeroPreferencial"
-  );
+  let ultimaSenha = document.getElementById("ultimaSenhaNumero");
 
-  callNormal(ultimaSenhaNormal);
-  callPreferencial(ultimaSenhaPreferencial);
+  callPassword(ultimaSenha);
 });
 
-function callNormal(passwordCall) {
+function callPassword(passwordCall) {
   // Adiciona um ouvinte de evento ao botão "Normal"
   document
     .getElementById("proximaNormal")
     .addEventListener("click", function () {
       // Obtém elementos do DOM necessários para manipulação
       let senhaAtual = document.getElementById("senhaAtualNumero");
-      let senhaNormal = document.getElementById("senhaNormal");
+      let ultimaSenha = document.getElementById("senhaAnterior");
       let audioChamada = document.getElementById("audioChamada");
+
+      console.log(senhaAtual);
 
       // Atualiza a última senha normal e gera uma nova senha
       passwordCall.innerHTML = senhaAtual.innerHTML;
 
-      let senha = parseInt(senhaNormal.value) + 1;
+      let senha = parseInt(ultimaSenha.value) + 1;
 
       senhaAtual.innerHTML = select(senha, 4);
-      senhaNormal.value = select(senha, 4);
+      ultimaSenha.value = select(senha, 4);
 
       // Reproduz o áudio de chamada
       audioChamada.play();
@@ -49,61 +47,14 @@ function callNormal(passwordCall) {
     .addEventListener("click", function () {
       // Obtém elementos do DOM necessários para manipulação
       let senhaAtual = document.getElementById("senhaAtualNumero");
-      let senhaNormal = document.getElementById("senhaNormal");
-
-      // Atualiza a próxima senha normal
-      passwordCall.innerHTML = select(
-        parseInt(document.getElementById("senhaNormal").value) + 1,
-        4
-      );
+      let ultimaSenha = document.getElementById("senhaAnterior");
 
       // Atualiza a última senha normal e gera uma nova senha decrementada
       passwordCall.innerHTML = senhaAtual.innerHTML;
-      let senha = parseInt(senhaNormal.value) - 1;
+
+      let senha = parseInt(ultimaSenha.value) - 1;
+
       senhaAtual.innerHTML = select(senha, 4);
-      senhaNormal.value = select(senha, 4);
-    });
-}
-
-function callPreferencial(passwordCall) {
-  //Adiciona um ouvinte de evento ao botão "Preferencial"
-
-  document
-    .getElementById("proximaPreferencial")
-    .addEventListener("click", function () {
-      // Obtém elementos do DOM necessários para manipulação
-      let senhaAtual = document.getElementById("senhaAtualNumero");
-      let senhaPrior = document.getElementById("senhaPrioridade");
-      let audioChamada = document.getElementById("audioChamada");
-
-      // Atualiza a última senha preferencial e gera uma nova senha de prioridade
-      passwordCall.innerHTML = senhaAtual.innerHTML;
-      let senha = parseInt(senhaPrior.value.replace("P", "")) + 1;
-      senhaAtual.innerHTML = "P" + select(senha, 3);
-      senhaPrior.value = "P" + select(senha, 3);
-
-      // Reproduz o áudio de chamada
-      audioChamada.play();
-    });
-
-  // Adiciona um ouvinte de evento ao botão "Anterior Preferencial"
-  document
-    .getElementById("anteriorPreferencial")
-    .addEventListener("click", function () {
-      // Obtém elementos do DOM necessários para manipulação
-      let senhaAtual = document.getElementById("senhaAtualNumero");
-      let senhaPrior = document.getElementById("senhaPrioridade");
-
-      // Atualiza a próxima senha preferencial
-      passwordCall.innerHTML = select(
-        parseInt(document.getElementById("senhaPrioridade").value) + 1,
-        4
-      );
-
-      // Atualiza a última senha preferencial e gera uma nova senha de prioridade decrementada
-      passwordCall.innerHTML = senhaAtual.innerHTML;
-      let senha = parseInt(senhaPrior.value.replace("P", "")) - 1;
-      senhaAtual.innerHTML = "P" + select(senha, 3);
-      senhaPrior.value = "P" + select(senha, 3);
+      ultimaSenha.value = select(senha, 4);
     });
 }
